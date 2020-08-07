7c973930da608e11dd799e25fb2483751ef06218
DmitryKachalov <rubyzza.dev@gmail.com>
2020-08-07 22:40:41 +0200

    Add test model Answer Question with specs

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+  it { should have_many(:answers).dependent(:destroy) }
+end
+
+RSpec.describe Answer, type: :model do
+  it { should validate_presence_of :body }
+  it { should belong_to :question }
+  it { should have_db_index :question_id }
 end
